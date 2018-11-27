44278140f16de2d2f5e664d5afc661fd9fbf9064
Mark Roxberry <roxberry@outlook.com>
2018-11-26 22:33:52 -0500

    clean up

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require('rootpath')();
-var express = require('express')
+const config = require('config/config');
+var express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jwt = require('_utils/jwt');
+//const jwt = require('_utils/jwt');
 const errorHandler = require('_utils/error-handler');
-const config = require('config/config');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// use JWT auth to secure the api
-app.use(jwt());
+// // use JWT auth to secure the api
+// app.use(jwt());
 
-// api routes
-app.use('/users', require('users/user.controller'));
+// // api routes
+// app.use('/users', require('users/user.controller'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
